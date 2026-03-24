1594a011b5d274a00b3e8fb0f8994aa8cd4d7c8f
safenuo <51493489+safenuo@users.noreply.github.com>
2026-03-24 03:32:51 -0300

    recuperação de senha
    Updated URL patterns for user authentication, including signup, login, profile, and password reset routes.

--- user/urls.py
+++ user/urls.py
@@ -1,21 +1,25 @@
 from django.urls import path
 from .views import *
 from django.contrib.auth import views as auth_views
-from django.views.generic import TemplateView
-
 
 urlpatterns = [
-    path("accounts/signup/", RegisterView.as_view(), name="SignUp"), # formata o singup
-    path('accounts/login/', CustomLoginView.as_view(redirect_authenticated_user=True, template_name='user/sign_in.html', authentication_form=LoginForm), name='login'),
-    path("accounts/profile/", ProfileView.as_view(), name="profile"), #formata o profile
+    # Rota de Cadastro
+    path("accounts/signup/", RegisterView.as_view(), name="SignUp"),
+    
+    # Rota de Login (Utiliza o formulário customizado do Tabler)
+    path('accounts/login/', CustomLoginView.as_view(
+        redirect_authenticated_user=True, 
+        template_name='user/sign_in.html', 
+        authentication_form=LoginForm
+    ), name='login'),
+    
+    # Rota de Perfil do Usuário
+    path("accounts/profile/", ProfileView.as_view(), name="profile"),
+    
+    # Rota de Logout
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('accounts/password-reset/', ResetPasswordView.as_view(), name='password_reset'), #reset
-    path('accounts/password-reset-confirm/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'),
-         name='password_reset_confirm'), #confirm
-    path(
-        'accounts/password-reset-complete/',
-        auth_views.PasswordResetCompleteView.as_view(template_name='user/password_reset_complete.html'),
-        name='password_reset_complete'), #complete
+    
+    # NOVA ROTA: Recuperação de Senha Facilitada (Direta no Site)
+    # Esta rota agora gerencia tanto a verificação do e-mail quanto a troca da senha
+    path('accounts/password-reset/', ResetPasswordView.as_view(), name='password_reset'),
 ]
-
